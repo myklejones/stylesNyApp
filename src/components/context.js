abfd55ec9ad33de9252f87bccd458e9a3aaca691
mykle jones <mykle.j91@gmail.com>
2020-04-15 21:04:54 -0400

    context

---
+++ src/components/context.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const InfoContext = React.createContext();
+
+class InfoProvider extends Component {
+    render() {
+        return (
+            <InfoContext.Provider value="Hello World" >
+                {this.props.children}
+            </InfoContext.Provider >
+        )
+    }
+}
+
+const InfoConsumer = InfoContext.Consumer;
+
+export { InfoProvider, InfoConsumer}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { InfoProvider } from "./components/context";
 
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
-, document.getElementById('root'));
-
+  <InfoProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </InfoProvider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
