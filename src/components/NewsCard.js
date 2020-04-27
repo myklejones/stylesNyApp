4747d3c4b79dbc17b348d483e5dd3991edf07a4d
mykle jones <mykle.j91@gmail.com>
2020-04-27 18:07:22 -0400

    news component

---
+++ src/components/NewsCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {InfoConsumer} from '../components/context'
+import {Link} from 'react-router-dom'
+
+class NewsCard extends Component {
+    render() {
+
+        const {id, newsTitle, newsText} = this.props.item;
+        return (
+            <InfoConsumer>
+                {value => (
+                    <div className="card container mt-2 mb-3 p-5">
+                        <div className="card-body">
+                            <h5 key={id} className="card-title">{newsTitle}</h5>
+                            <p className="crd-text">{newsText}</p>
+                            <Link className="card-Link">Read More >></Link>
+                        </div>
+                    </div>
+        )}
+                
+            </InfoConsumer>
+  
+        )
+    }
+}
+
+export default NewsCard;
