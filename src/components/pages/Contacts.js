d813457d18a9e05511df4915ee2c7331903554a6
mykle jones <mykle.j91@gmail.com>
2020-04-29 21:09:18 -0400

    contact us page

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -3,11 +3,20 @@ import React, { Component } from 'react'
 class Contacts extends Component {
     render() {
         return (
-            <div>
-                <h2>Contacts page</h2>
-            </div>
+            <section className="my-5 py-5">
+                <div className="container">
+                    <div className="well well-sm">
+                        <h3><strong>Our Location</strong></h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-7">
+
+                    </div>
+                </div>
+            </section>
         )
     }
 }
 
-export default Contacts;
+export default Contacts; 
